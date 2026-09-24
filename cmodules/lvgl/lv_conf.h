3ac33935115f65812dd4dcf5b276023782ae7eab
Yeqin Gong <yeqin.gong@qq.com>
2026-09-24 15:30:56 +0800

    添加自定义字体

--- cmodules/lvgl/lv_conf.h
+++ cmodules/lvgl/lv_conf.h
@@ -641,10 +641,10 @@ extern void mp_lv_deinit_gc();
  *  #define LV_FONT_CUSTOM_DECLARE   LV_FONT_DECLARE(my_font_1) LV_FONT_DECLARE(my_font_2)
  *  @endcode
  */
-#define LV_FONT_CUSTOM_DECLARE
+#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(font_puhui_14_1) LV_FONT_DECLARE(font_puhui_16_4)
 
 /** Always set a default font */
-#define LV_FONT_DEFAULT &lv_font_montserrat_14
+#define LV_FONT_DEFAULT &font_puhui_14_1
 
 /** Enable handling large font and/or fonts with a lot of characters.
  *  The limit depends on the font size, font face and bpp.
--- cmodules/lvgl/micropython.cmake
+++ cmodules/lvgl/micropython.cmake
@@ -1,3 +1,11 @@
 set(LV_CONF_PATH ${CMAKE_CURRENT_LIST_DIR}/lv_conf.h)
 
 include(${MICROPY_DIR}/../lv_binding/micropython.cmake)
+
+# Puhui Fonts from https://github.com/78/xiaozhi-fonts
+add_library(usermod_lvgl_font INTERFACE)
+target_sources(usermod_lvgl_font INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/font_puhui_14_1.c
+    ${CMAKE_CURRENT_LIST_DIR}/font_puhui_16_4.c
+)
+target_link_libraries(usermod INTERFACE usermod_lvgl_font)
